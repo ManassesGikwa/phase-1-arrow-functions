06667e189b07f5db970cb5116b9aba682b80909c
ManassesGikwa <manassesgikwa@gmail.com>
2023-12-15 16:52:16 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,8 @@
+// divide divides 2000 by 100
+const divide = x => 2000/100;
+
+//has an arrow function called square
+const square = x => x * x;
+//has an arrow function called square
+const add = (x,y)=> x +y;
+
